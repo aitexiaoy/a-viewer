42e686b8edb26f39eb4a27afab31ec690bc63bed
yangpeng <aitexiaoy@163.com>
2021-03-12 15:32:17 +0800

    feat: 修改输出路径

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     title: 'SVGA、Lottie动画在线预览',
     description: 'SVGA、Lottie动画在线预览',
-    dest: "dist",
+    dest: "docs",
     head: [
         ['script', { src: 'https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js' }],
         ['script', { src: 'https://cdn.jsdelivr.net/npm/svgaplayerweb@2.3.1/build/svga.min.js' }],
